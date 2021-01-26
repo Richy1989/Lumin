78a4a9e80746e1b8854dbff077afc0b06ba22901
Richard Leopold <richy.leopold@outlook.com>
2021-01-26 18:20:55 +0100

    updated installation script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 #Write Service File Function
 #Parameters: 1. servicePath 2. installPath 3. dotnetPath 4. DllName 5. userName
 WriteServiceFile () {
+	local servicePath 
+	local installPath
+	local dotnetPath
+	local DllName
+	local userName
 	servicePath=$1
 	installPath=$2
 	dotnetPath=$3
@@ -24,6 +29,35 @@ WriteServiceFile () {
 	echo '[Install]' >> $servicePath
 	echo 'WantedBy=multi-user.target' >> $servicePath
 }
+#Creates a Directory and sets the permissions and owing to the selected user
+#Parameters: 1. Folder Path, 2. User
+MakeDirWithUserPermissions () {
+	local folder 
+	local userName 
+	folder=$1
+	userName = $2
+	
+	mkdir $folder
+	chown $userName":"$userName $usernName
+	chmod -R 0755 $folder
+}
+
+#Checks if a programm exists
+#Parameters: 1. Program to check
+CheckForProgram () {
+	local program 
+	program="${1}"
+
+	#printf "Checking for ${program}\\n  "
+	command -v "${program}"
+
+	if [[ "${?}" -ne 0 ]]; then
+		#printf "${program} is not installed, exiting\\n"
+		return 0
+	fi
+	
+	return 1
+}
 
 ##Main Program###
 
@@ -41,7 +75,8 @@ dotnetPath="/usr/share/dotnet"
 installPath=$homeFolder"/lumin/"
 
 luminConfigName="lumin.config"
-luminConfigPath=$homeFolder".luminConfig/"$luminConfigName
+luminConfigFolder=$homeFolder".luminConfig/"
+luminConfigPath=$luminConfigFolder$luminConfigName
 
 deviceRulesPath="/etc/udev/rules.d/"
 spiDeviceRule=$deviceRulesPath"50-spi.rules"
@@ -52,29 +87,36 @@ serverServicePath="/etc/systemd/system/"$serverServiceName
 discoveryPort=8080
 signalPort=5000
 
-
 hashOutput=$(hash dotnet)
-netCoreDownloadFileName="aspnetcore-runtime-5.0.0-linux-arm64.tar.gz"
+netCoreDownloadFileName="dotnet-sdk-5.0.102-linux-arm64.tar.gz"
 
 #check and install dotNet 5.0
-if [[ $hashOutput != "" ]]
+CheckForProgram "dotnet"
+if [ $? ]
 then
-	wget https://download.visualstudio.microsoft.com/download/pr/ac555882-afa3-4f5b-842b-c4cec2ae0e90/84cdd6d47a9f79b6722f0e0a9b258888/aspnetcore-runtime-5.0.0-linux-arm64.tar.gz
+	dotnetPath=$(whereis dotnet)
+else
+	wget https://download.visualstudio.microsoft.com/download/pr/4fdd4708-8990-42db-998d-36ccfa593070/d67cb90c382e4eedbca8af1aebcbbe19/dotnet-sdk-5.0.102-linux-arm64.tar.gz
 	mkdir $dotnetPath
 	export PATH=$PATH:$dotnetPath
+	export DOTNET_ROOT=$dotnetPath/dotnet
 	tar zxf $netCoreDownloadFileName -C $dotnetPath
 	rm $netCoreDownloadFileName
-else
-	dotnetPath=$(whereis dotnet)
 fi
 
-echo dotNet path is at: $dotnetPath 
+#Setting Symbolix link for dotnet
+ln "-s" $dotnetPath"/dotnet" "/usr/bin/dotnet"
+echo "dotNet path is at: "$dotnetPath 
 
 #open ports in firewall
 ufw allow $discoveryPort
 ufw allow $signalPort
 
 #write config file to: luminConfigPath
+
+#Create Config Folder
+MakeDirWithUserPermissions $luminConfigFolder $userName
+
 echo '#CONFIG FILE Start' > $luminConfigPath
 echo '#-------------------------------------------' >> $luminConfigPath
 echo '#Name of the Led Client' >> $luminConfigPath
@@ -100,18 +142,19 @@ apt install libgpiod2 -y
 WriteServiceFile $serverServicePath $installPath $dotnetPath $luminServerDllName $userName
 
 #Clone, publish and install binaries
-#installPath="/home/ubuntu/tempInstall"
-mkdir $installPath
-mkdir ".lumin_temp"
-cd ".lumin_temp"
+MakeDirWithUserPermissions $installPath $userName
+tempFolder=".lumin_temp"
+#Create Temp Folder
+MakeDirWithUserPermissions $tempFolder $userName
+cd $tempFolder
 git clone $releaseRepository
 dotnet publish "Lumin/LuminServer/LuminServer.csproj"
-actualFolder=pwd #$(pwd)
+actualFolder=$(pwd)
 cd "Lumin/LuminServer/bin/Debug/net5.0/publish/"
 cp -R * $installPath
 cd $actualFolder
 cd ".."
-rm -r -f ".lumin_temp"
+rm -r -f $tempFolder
 
 #Enable the Server Service
 systemctl enable $serverServiceName
